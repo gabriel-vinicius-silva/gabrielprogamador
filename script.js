6d997c2c8e50246b9ec1f4bc2fb6d025f738b9a7
gabriel-vinicius-silva <gabrieuvinici@gmail.com>
2026-04-23 20:42:35 -0300

    treta

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pequena interação: mensagem automática ao clicar no botão
+
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+  console.log("Cliente clicou em pedir orçamento");
+});
